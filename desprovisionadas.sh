47abebc81e31b7d20998ad1e8bc08eeda973d71d
JeanFerreira0 <101665692+JeanFerreira0@users.noreply.github.com>
2023-10-30 16:03:42 -0300

    Add files via upload

--- desprovisionadas.sh
+++ desprovisionadas.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# Desenvolvido por: Bee Solutions
+# Autor: Fernando Almondes
+# Data: 16/08/2023
+
 COMMUNITY=$1
 IP=$2
+PORTA_SNMP=$3
 
-snmpwalk -v2c -c $COMMUNITY $IP 1.3.6.1.4.1.2011.6.128.1.1.2.48.1.2 | grep -v "No Such" |  wc -l
+snmpwalk -v2c -c $COMMUNITY $IP:$PORTA_SNMP 1.3.6.1.4.1.2011.6.128.1.1.2.48.1.2 | grep -v "No Such" |  wc -l
